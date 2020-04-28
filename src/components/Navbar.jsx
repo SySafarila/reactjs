80e81b893371d878da36984d7f88c7799ff82b7d
Syahrul Safarila <sysafarila.official@gmail.com>
2020-04-28 11:43:10 +0700

    Update

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Navbar extends React.Component {
+    render() {
+        return (
+            <nav class="navbar navbar-dark bg-dark shadow-sm">
+                <span class="navbar-brand mb-0 h1">Navbar</span>
+            </nav>
+        );
+    }
+}
+
+export default Navbar;
--- src/main/Main.jsx
+++ src/main/Main.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 
 import ShoppingList from '../components/ShoppingList';
+import Navbar from '../components/Navbar';
+// import '../bootstrap.main.css';
 
 function Main() {
     return (
         <div>
-            <h1>This is main</h1>
-            <ShoppingList name="This is props" buttonTitle="The Button"/>
+            <Navbar />
+            <div className="container">
+                <h1>This is main</h1>
+                <ShoppingList name="This is props" buttonTitle="The Button"/>
+            </div>
         </div>
     );
 }
