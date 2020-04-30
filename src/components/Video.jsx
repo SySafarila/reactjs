7b9e59ab832fc04a290826b960b5a11d11c0e3c8
Syahrul Safarila <sysafarila.official@gmail.com>
2020-04-30 18:57:21 +0700

    Update

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -23,17 +23,19 @@ class Video extends React.Component {
 
     render() {
         return (
-            <div className="col-md-3 mb-2">
-                <img src={this.props.img} className="img-fluid mb-2" alt="Jomas's video" />
-                <div className="row">
-                    <div className="col-2 col-md-4">
-                        <img src={this.props.channelPhoto} className="img-thumbnail rounded-circle border-0" width="68" alt="Jomas's video" />
-                    </div>
-                    <div className="col p-0">
-                        <h6 className="mb-0">{this.props.title}</h6>
-                        <span className="text-muted d-block">{this.props.channel}</span>
-                        <span className="text-muted d-block">{this.props.viewsNumber} views · {this.props.time}</span>
-                        <span className="text-muted d-block">{this.state.timeRun}</span>
+            <div className="col-md-6 col-lg-4 mb-2">
+                <div className="card h-100">
+                    <img src={this.props.img} className="img-fluid mb-2" alt="Jomas's video" />
+                    <div className="card-body row p-0 pl-3">
+                        <div className="col-2 col-md-4 p-0 pr-md-4">
+                            <img src={this.props.channelPhoto} className="img-thumbnail border-0" alt="Jomas's video" />
+                        </div>
+                        <div className="col p-0">
+                            <h6 className="mb-0 pr-1">{this.props.title}</h6>
+                            <span className="text-muted d-block">{this.props.channel}</span>
+                            <span className="text-muted d-block">{this.props.viewsNumber} views · {this.props.time}</span>
+                            <span className="text-muted d-block">{this.state.timeRun}</span>
+                        </div>
                     </div>
                 </div>
                 <hr></hr>
