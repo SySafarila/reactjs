dc57e680f368d98424c46b02ea554d175d9fa297
Syahrul Safarila <sysafarila.official@gmail.com>
2020-04-30 19:05:46 +0700

    Update

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -25,12 +25,12 @@ class Video extends React.Component {
         return (
             <div className="col-md-6 col-lg-4 mb-2">
                 <div className="card h-100">
-                    <img src={this.props.img} className="img-fluid mb-2" alt="Jomas's video" />
+                    <img src={this.props.img} className="img-fluid mb-2" alt={this.props.alt} />
                     <div className="card-body row p-0 pl-3">
                         <div className="col-2 col-md-4 p-0 pr-md-4">
-                            <img src={this.props.channelPhoto} className="img-thumbnail border-0" alt="Jomas's video" />
+                            <img src={this.props.channelPhoto} className="img-thumbnail rounded-circle border-0" alt={this.props.alt} />
                         </div>
-                        <div className="col p-0">
+                        <div className="col pl-0 pr-2">
                             <h6 className="mb-0 pr-1">{this.props.title}</h6>
                             <span className="text-muted d-block">{this.props.channel}</span>
                             <span className="text-muted d-block">{this.props.viewsNumber} views · {this.props.time}</span>
