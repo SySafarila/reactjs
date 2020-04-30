7af6f19012715efeb5aa62df5443f6ae95307a18
Syahrul Safarila <sysafarila.official@gmail.com>
2020-05-01 05:05:32 +0700

    update

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -23,14 +23,15 @@ class Video extends React.Component {
 
     render() {
         return (
-            <div className="col-md-6 col-lg-4 mb-2">
-                <div className="card h-100">
+            <div className="col-md-6 col-lg-4 mb-3">
+                <div className="card h-100 shadow-sm border-0">
+                    <a href={this.props.link} className="stretched-link" target="_blank" rel="noopener noreferrer"><span className="d-none">Go to video</span></a>
                     <img src={this.props.img} className="img-fluid mb-2" alt={this.props.alt} />
                     <div className="card-body row p-0 pl-3">
                         <div className="col-2 col-md-3 p-0 pr-md-1">
                             <img src={this.props.channelPhoto} className="img-thumbnail rounded-circle border-0" alt={this.props.alt} />
                         </div>
-                        <div className="col pl-1">
+                        <div className="col pl-1 mb-2">
                             <h6 className="mb-0">{this.props.title}</h6>
                             <span className="text-muted d-block">{this.props.channel}</span>
                             <span className="text-muted d-block">{this.props.viewsNumber} views · {this.props.time}</span>
@@ -38,7 +39,6 @@ class Video extends React.Component {
                         </div>
                     </div>
                 </div>
-                <hr></hr>
             </div>
         )
     }
